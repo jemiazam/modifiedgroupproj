6dc04f9ca5cb66a8a366ec48b2c57366b1c730ba
MohamadHvzim <hazim8669@gmail.com>
2024-01-19 00:32:57 +0800

    members slayyy

---
+++ resources/views/members.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Tables / Data - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Jan 09 2024 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+
+
+    <!-- Default Table -->
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Position</th>
+            <th scope="col">Age</th>
+            <th scope="col">Start Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Brandon Jacob</td>
+            <td>Designer</td>
+            <td>28</td>
+            <td>2016-05-25</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Bridie Kessler</td>
+            <td>Developer</td>
+            <td>35</td>
+            <td>2014-12-05</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Ashleigh Langosh</td>
+            <td>Finance</td>
+            <td>45</td>
+            <td>2011-08-12</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Angus Grady</td>
+            <td>HR</td>
+            <td>34</td>
+            <td>2012-06-11</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Raheem Lehner</td>
+            <td>Dynamic Division Officer</td>
+            <td>47</td>
+            <td>2011-04-19</td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- End Default Table Example -->
+    </body>
